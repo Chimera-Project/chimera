d0dd0b86a555fdbc06e03362148ba5a4479aaa94
pxckets <49133592+pxckets@users.noreply.github.com>
2019-08-03 00:14:17 -0400

    Some little things

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -5,21 +5,22 @@
 #pragma once
 
 const std::string windowsAsciiArt =
-      "\n_____             _  _  _         _ \n"        
+     " _____             _  _  _         _ \n"        
      "|  _  |           (_)| || |       | |\n"        
      "| | | | ___   ___  _ | || |  __ _ | |_   ___ \n"
      "| | | |/ __| / __|| || || | / _` || __| / _ \\\n"
-     "\\ \\_/ /\\__ \\| (__ | || || || (_| || |_ |  __/\n"
-     " \\___/ |___/ \\___||_||_||_| \\__,_| \\__| \\___|\n";
+     " \\ \\_/ /\\__ \\| (__ | || || || (_| || |_ |  __/\n"
+     "  \\___/ |___/ \\___||_||_||_| \\__,_| \\__| \\___|\n";
 
 
 const std::string nonWindowsAsciiArt =
-     "\n _____             _  _  _         _ \n"
-     "|  _  |           (_)| || |       | |\n"
+     " _____             _  _  _         _ \n"        
+     "|  _  |           (_)| || |       | |\n"        
      "| | | | ___   ___  _ | || |  __ _ | |_   ___ \n"
      "| | | |/ __| / __|| || || | / _` || __| / _ \\\n"
-     "\\ \\_/ /\\__ \\| (__ | || || || (_| || |_ |  __/\n"
-     " \\___/ |___/ \\___||_||_||_| \\__,_| \\__| \\___|\n";
+     " \\ \\_/ /\\__ \\| (__ | || || || (_| || |_ |  __/\n"
+     "  \\___/ |___/ \\___||_||_||_| \\__,_| \\__| \\___|\n";
+
 /* Windows has some characters it won't display in a terminal. If your ascii
    art works fine on Windows and Linux terminals, just replace 'asciiArt' with
    the art itself, and remove these two #ifdefs and above ascii arts */
