516a1ffe8bd786b8ba03b442fcd0c6ab47c52b90
Tobias aka. TheDevMinerTV <tobigames200@gmail.com>
2019-08-17 21:32:49 +0200

    Fix ASCII header

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -3,29 +3,10 @@
 // Please see the included LICENSE file for more information
 //Thanks to  arifyan for fixing it
 #pragma once
-
-const std::string windowsAsciiArt =
-     " _____             _  _  _         _ \n"        
-     "|  _  |           (_)| || |       | |\n"        
-     "| | | | ___   ___  _ | || |  __ _ | |_   ___ \n"
-     "| | | |/ __| / __|| || || | / _` || __| / _ \\\n"
-     " \\ \\_/ /\\__ \\| (__ | || || || (_| || |_ |  __/\n"
-     "  \\___/ |___/ \\___||_||_||_| \\__,_| \\__| \\___|\n";
-
-
-const std::string nonWindowsAsciiArt =
-     " _____             _  _  _         _ \n"        
-     "|  _  |           (_)| || |       | |\n"        
-     "| | | | ___   ___  _ | || |  __ _ | |_   ___ \n"
-     "| | | |/ __| / __|| || || | / _` || __| / _ \\\n"
-     " \\ \\_/ /\\__ \\| (__ | || || || (_| || |_ |  __/\n"
-     "  \\___/ |___/ \\___||_||_||_| \\__,_| \\__| \\___|\n";
-
-/* Windows has some characters it won't display in a terminal. If your ascii
-   art works fine on Windows and Linux terminals, just replace 'asciiArt' with
-   the art itself, and remove these two #ifdefs and above ascii arts */
-#ifdef _WIN32
-const std::string asciiArt = windowsAsciiArt;
-#else
-const std::string asciiArt = nonWindowsAsciiArt;
-#endif
+const std::string asciiArt =
+"  _____             _  _  _         _ \n"
+" |  _  |           (_)| || |       | |\n"
+" | | | | ___   ___  _ | || |  __ _ | |_   ___\n"
+" | | | |/ __| / __|| || || | / _` || __| / _ \\\n"
+" \\ \\_/ /\\__ \\| (__ | || || || (_| || |_ |  __/\n"
+"  \\___/ |___/ \\___||_||_||_| \\__,_| \\__| \\___|\n";
