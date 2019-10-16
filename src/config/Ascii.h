f129bd5c7a0db587c1990554ab654510def14c36
sudo-root <56373872+Chimera-Project@users.noreply.github.com>
2019-10-16 13:43:57 -0600

    Update Ascii.h

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -1,12 +1,25 @@
-// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2019, The Chimera Developers
 //
 // Please see the included LICENSE file for more information
 //Thanks to  arifyan for fixing it
 #pragma once
 const std::string asciiArt =
-"  _____             _  _  _         _ \n"
-" |  _  |           (_)| || |       | |\n"
-" | | | | ___   ___  _ | || |  __ _ | |_   ___\n"
-" | | | |/ __| / __|| || || | / _` || __| / _ \\\n"
-" \\ \\_/ /\\__ \\| (__ | || || || (_| || |_ |  __/\n"
-"  \\___/ |___/ \\___||_||_||_| \\__,_| \\__| \\___|\n";
+"                                     \n"
+"                                                                                                            \n"                   
+"       CCCCCCCCCCCCC MMMMMMMM               MMMMMMMM RRRRRRRRRRRRRRRRR                    AAA               \n"
+"     CCC::::::::::::C M:::::::M             M:::::::M R::::::::::::::::R                 A:::A              \n"
+"   CC:::::::::::::::C M::::::::M           M::::::::M R::::::RRRRRR:::::R               A:::::A             \n"
+"  C:::::CCCCCCCC::::C M:::::::::M         M:::::::::M RR:::::R     R:::::R             A:::::::A            \n"
+" C:::::C       CCCCCC M::::::::::M       M::::::::::M   R::::R     R:::::R            A:::::::::A           \n"
+"C:::::C               M:::::::::::M     M:::::::::::M   R::::R     R:::::R           A:::::A:::::A          \n"
+"C:::::C               M:::::::M::::M   M::::M:::::::M   R::::RRRRRR:::::R           A:::::A A:::::A         \n"
+"C:::::C               M::::::M M::::M M::::M M::::::M   R:::::::::::::RR           A:::::A   A:::::A        \n"
+"C:::::C               M::::::M  M::::M::::M  M::::::M   R::::RRRRRR:::::R         A:::::A     A:::::A       \n"
+"C:::::C               M::::::M   M:::::::M   M::::::M   R::::R     R:::::R       A:::::AAAAAAAAA:::::A      \n"
+"C:::::C               M::::::M    M:::::M    M::::::M   R::::R     R:::::R      A:::::::::::::::::::::A     \n"
+" C:::::C       CCCCCC M::::::M     MMMMM     M::::::M   R::::R     R:::::R     A:::::AAAAAAAAAAAAA:::::A    \n"
+"  C:::::CCCCCCCC::::C M::::::M               M::::::M RR:::::R     R:::::R    A:::::A             A:::::A   \n"
+"   CC:::::::::::::::C M::::::M               M::::::M R::::::R     R:::::R   A:::::A               A:::::A  \n"
+"     CCC::::::::::::C M::::::M               M::::::M R::::::R     R:::::R  A:::::A                 A:::::A \n" 
+"        CCCCCCCCCCCCC MMMMMMMM               MMMMMMMM RRRRRRRR     RRRRRRR AAAAAAA                   AAAAAAA\n"
+"                                                                                                            \n"; 
