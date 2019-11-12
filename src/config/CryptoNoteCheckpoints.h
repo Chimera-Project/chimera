62520e7a0691714bc6f8a2565ec58e5b60c63d78
sudo-root <56373872+Chimera-Project@users.noreply.github.com>
2019-11-12 16:00:37 -0700

    Update CryptoNoteCheckpoints.h
    Update checkpoints

--- src/config/CryptoNoteCheckpoints.h
+++ src/config/CryptoNoteCheckpoints.h
@@ -26,5 +26,13 @@ struct CheckpointData {
   const char* blockId;
 };
 
-const std::initializer_list<CheckpointData> CHECKPOINTS = {};
-}
+const std::initializer_list<CheckpointData> CHECKPOINTS = {
+        {0, "05dc0aa892144dc69dab8ab0637e3015bd4db56ad3cc8fa17d3ddac941163faa"},
+        {2, "8c00a064e676bc24108b27f9c16021887f23a97dcd7e779a8bdaa4bad3c06837"},
+        {5000, "2a4fa21bf5a39f902043d9a5c1785ba0e0800f2e66d5400d7e2c81856b798222"},
+        {10000, "bc98901708c869f12f3b9acd4e6bc59cd14224b998f444af8da019ff7fac23da"},
+        {15000, "5de6dd58d67526d46b85c3aacdf615b77042de5a65c20321606916764c812207"},
+        {20000, "bd5395f7dde0d0c77e8cf7f148c0b1b59cb77deb86066313e67465e047e22d71"},
+        {25000, "9456f83d7983debc9a1488766501f9d5e0dab65af161da154c23be18bd19b66e"},
+        {30000, "1ffcb59031845aa13bb04d23d101ed683b209e6e07eca2a3755f2e1e87c25b14"}};
+} // namespace CryptoNote
