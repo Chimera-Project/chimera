7dedfef7e0ad9585e2559c3fb5ace1b863d4b958
sudo-root <56373872+Chimera-Project@users.noreply.github.com>
2019-12-01 15:38:22 -0700

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -282,5 +282,6 @@ const char* const SEED_NODES[] = {
   "95.179.188.118:11245", //Europe seed node 
   "207.148.77.219:11245", //East Asia seed node and block explorer
   "75.174.142.9:11245", //Cryptodude seed node
+  "3.225.124.85:11245", //Tyler seed node
 };
 } // CryptoNote
