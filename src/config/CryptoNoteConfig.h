0d49efdddf10a09307a6dc2f070076992e39866e
sudo-root <56373872+Chimera-Project@users.noreply.github.com>
2019-12-04 23:35:49 -0700

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -117,7 +117,7 @@ const uint32_t MIXIN_LIMITS_V3_HEIGHT                        = 60;
 const uint32_t MIXIN_LIMITS_V4_HEIGHT                        = 30000; 
 const uint32_t MIXIN_LIMITS_V5_HEIGHT                        = 38000;
 const uint32_t MIXIN_LIMITS_V6_HEIGHT                        = 65000;
-const uint32_t MIXIN_LIMITS_V3_HEIGHT                        = 75000;
+const uint32_t MIXIN_LIMITS_V3_HEIGHT                        = 72000;
   
 
 /* The mixin to use by default with zedwallet and turtle-service */
@@ -127,7 +127,7 @@ const uint64_t DEFAULT_MIXIN_V1                              = MAXIMUM_MIXIN_V1;
 const uint64_t DEFAULT_MIXIN_V2                              = MAXIMUM_MIXIN_V2;
 const uint64_t DEFAULT_MIXIN_V3                              = MINIMUM_MIXIN_V3;
 const uint64_t DEFAULT_MIXIN_V5                              = MAXIMUM_MIXIN_V5;
-const uint64_t DEFAULT_MIXIN_V6                              = 0;
+const uint64_t DEFAULT_MIXIN_V6                              = MINIMUM_MIXIN_V6;
 const uint64_t DEFAULT_MIXIN_V7                              = MAXIMUM_MIXIN_V7;
 
 const uint64_t DEFAULT_DUST_THRESHOLD                        = UINT64_C(10);
