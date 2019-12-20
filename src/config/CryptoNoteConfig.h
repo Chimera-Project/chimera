0db792ef2c794e4b08f1f98e4d6aa87194713aed
YetAnotherRussian <4670472+YetAnotherRussian@users.noreply.github.com>
2019-12-20 12:32:59 +0300

    Fix MSVC build

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -186,10 +186,12 @@ static_assert(0 < UPGRADE_VOTING_THRESHOLD && UPGRADE_VOTING_THRESHOLD <= 100, "
 static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 
 /* Block heights we are going to have hard forks at */
-const uint64_t FORK_HEIGHTS[] =
-{
-     
-};
+/* MSVC compiler does not support zero-length arrays - see https://gcc.gnu.org/onlinedocs/gcc/Zero-Length.html */
+#ifdef _MSC_VER
+const uint64_t FORK_HEIGHTS[] = {0};
+#else
+const uint64_t FORK_HEIGHTS[] = {};
+#endif
 
 /* MAKE SURE TO UPDATE THIS VALUE WITH EVERY MAJOR RELEASE BEFORE A FORK */
 const uint64_t SOFTWARE_SUPPORTED_FORK_INDEX                 = 0;
